51ccd9b6e3fda2e08f4ea96879462d7f1e17ec25
Osama <m.eltantawi99@gmail.com>
2025-06-26 13:32:02 +0200

    comment out default theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,14 +27,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased`}>
-        <ThemeProvider
+        {/* <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-        >
+        > */}
           {children}
-        </ThemeProvider>
+        {/* </ThemeProvider> */}
       </body>
     </html>
   );
