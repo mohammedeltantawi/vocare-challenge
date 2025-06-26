cc7d61ce83f90ced9ad5463b65adcdb30d6933cb
Osama <m.eltantawi99@gmail.com>
2025-06-26 15:07:16 +0200

    style: add icons and adjust color for appointment card

--- components/appointment-card.component.tsx
+++ components/appointment-card.component.tsx
@@ -1,4 +1,5 @@
 import { Appointment } from "@/models/appointment.model";
+import { Clock, MapPin, MessageSquareText } from "lucide-react";
 
 interface Props {
   appointment: Appointment;
@@ -6,11 +7,24 @@ interface Props {
 
 export default function AppointmentCard({ appointment }: Props) {
   return (
-    <div className="absolute left-1 right-1 rounded-md shadow-sm p-2 text-sm bg-gray-100">
+    <div className="absolute left-1 right-1 rounded-md shadow-sm p-2 text-sm bg-secondary">
       <strong className="block">{appointment.title}</strong>
-      <p className="text-xs text-gray-600">{new Date(appointment.start).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} - {new Date(appointment.end).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
-      {appointment.location && <p className="text-xs text-gray-500">{appointment.location}</p>}
-      {appointment.notes && <p className="text-xs text-gray-500">{appointment.notes}</p>}
+      <div className="flex flex-row gap-1 items-center">
+        <Clock color="var(--secondary-text)" size={12}></Clock>
+        <p className="text-xs text-secondaryText">{new Date(appointment.start).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} - {new Date(appointment.end).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+      </div>
+      {appointment.location && 
+      <div className="flex flex-row gap-1 items-center">
+        <MapPin color="var(--secondary-text)" className="flex-shrink-0" size={12}></MapPin>
+        <p className="text-xs text-secondaryText whitespace-normal break-words">{appointment.location}</p>
+      </div>
+      }
+      {appointment.notes &&
+      <div className="flex flex-row gap-1 items-center">
+        <MessageSquareText color="var(--secondary-text)" className="flex-shrink-0" size={12}></MessageSquareText>
+        <p className="text-xs text-secondaryText whitespace-normal break-words">{appointment.notes}</p>
+      </div>
+       }
     </div>
   );
 }
